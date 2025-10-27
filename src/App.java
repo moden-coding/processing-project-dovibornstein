2ecf01dc968db02a3502a9ab370f79cf837e4e74
dovibornstein <dovi.bornstein@gmail.com>
2025-10-27 12:04:49 -0400

    in progress

--- src/App.java
+++ src/App.java
@@ -15,7 +15,17 @@ public class App extends PApplet {
 
     float speed = 5;
 
+    float rectX = 50;
+    float rectY = 500;
+    float rectW = 30;
+    float rectH = 60;
+
+    float velocity;
+    float accelleration;
+
     public void setup() {
+        accelleration = 0.1f;
+        velocity = 3;
         background(255, 255, 255);
         strokeWeight(3);
         line(0, 500, 800, 500);
@@ -29,33 +39,45 @@ public class App extends PApplet {
     public void draw() {
         background(255, 255, 255);
 
-        float x1a = triX1;
+        line(0, 560, 800, 560);
+
         float y1a = triY1;
-        triangle(x1a + 17.5f, y1a - 17.5f, x1a, y1a - 35, x1a, y1a);//x1a, y1a, x1a + 35, y1a, x1a + 17.5, y1a - 35);
+        triangle(triX1 - 35, y1a - 17.5f, triX1, y1a - 35, triX1, y1a);
 
         float x1b = triX2;
         float y1b = triY2;
-        triangle(x1b, y1b, x1b + 35, y1b, x1b + 17.5f, y1b - 35); // do the same thing once you figure out
+        triangle(x1b - 35, y1b - 17.5f, x1b, y1b - 35, x1b, y1b);
 
         triX1 -= speed;
         triX2 -= speed;
 
-        if (x1a + 17.5 < 0) {
-            x1a = + 17.5f  + width;
-    
-        float randomx1a =   17.5f + random(1600, 800);
-        println(randomx1a, y1a, x1a + 35, y1a, x1a + 17.5, y1a - 35);
+        if (triX1 < 0) {
+            triX1 = random(1000, 1600);
+            println(triX1);
         }
 
-        // if (x1b + 17.5 < 0) {
-        //     x1b = + 17.5f + width;
-        
-        // float randomx2b = 17.5f + random(1600, 800);
-        // println(randomx1b, y1b, x1b + 35, y1b, x1b + 17.5, y1b - 35);
-        // }
-        
+        if (triX2 < 0) {
+            triX2 = random(1000, 1600);
+            println(triX2);
+        }
 
-        
+        velocity += accelleration;
+        rectY += velocity;
+
+        if (rectY >= 500) {
+            rectY = 500;
+            velocity = 0;
+        }
+
+        rect(rectX, rectY, rectW, rectH);
 
     }
+
+    public void keyPressed() {
+    if (key == ' ') {
+        velocity = -7;
+    }
+
+    }
+
 }
